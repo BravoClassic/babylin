ed0e1e40498662203e612706062dad8aafe4cef7
BravoClassic <gerald.akorli@gmail.com>
2020-07-22 10:56:19 +0000

    Working on the new Stock Page

--- src/babylinapp/NewStockPageController.java
+++ src/babylinapp/NewStockPageController.java
@@ -1,20 +1,61 @@
 package babylinapp;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class NewStockPageController {
-    public TextField newStockNameProductPage;
-    public TextField newStockPriceStockPage;
-    public TextField newStockQuantityStockPage;
-    public TextArea newStockDescStockPage;
-    public Button cancel;
 
-    public void newStock(ActionEvent event) {
+    @FXML
+    protected Button newBtnStock;
+
+    @FXML
+    protected TextField newStockNameProductPage;
+
+    @FXML
+    protected TextField newStockPriceStockPage;
+
+    @FXML
+    protected TextField newStockQuantityStockPage;
+
+    @FXML
+    protected TextArea newStockDescStockPage;
+
+    @FXML
+    protected Button cancel;
+
+    public void newStock() {
+        try {
+            Connection connection = DriverManager.getConnection(jdbcController.url, jdbcController.user, jdbcController.password);
+            PreparedStatement preparedStatement = connection.prepareStatement(jdbcController.INSERT_QUERY_STOCK);
+            preparedStatement.setString(0,newStockNameProductPage.getText());
+            preparedStatement.setString(1,newStockPriceStockPage.getText());
+            preparedStatement.setString(2,newStockQuantityStockPage.getText());
+            preparedStatement.setString(3,newStockDescStockPage.getText());
+
+            boolean resultSet = preparedStatement.execute();
+
+            if (resultSet){
+                Controller.infoBox("New Stock has been added successfully!",null,"Success!");
+            }else {
+                Controller.infoBox("New Stock has not been added successfully!",null, "Failed!");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void goBack(ActionEvent event) {
+    public void goBack() throws IOException {
+        Stage stage = (Stage) cancel.getScene().getWindow();
+        new Menu().start(stage);
     }
 }
