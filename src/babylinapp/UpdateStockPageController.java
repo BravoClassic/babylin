9a13be3ebfc3ecdb47a19e7b4d2118d5a286551f
BravoClassic <gerald.akorli@gmail.com>
2020-07-22 12:46:18 +0000

    Worked on the functionality of the cancel button

--- src/babylinapp/UpdateStockPageController.java
+++ src/babylinapp/UpdateStockPageController.java
@@ -1,4 +1,20 @@
 package babylinapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class UpdateStockPageController {
+
+    @FXML
+    protected Button cancel;
+
+    @FXML
+    protected void goBack() throws IOException {
+        Stage stage = (Stage) cancel.getScene().getWindow();
+        new stocks().start(stage);
+    }
 }
